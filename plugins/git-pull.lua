d02a155cdcbdb0d8ea8d4287fc0936c59a92e9e4
daurnimator <quae@daurnimator.com>
2015-12-17 22:39:42 +1100

    plugins/git-pull: print exit code instead of os.execute first return value

--- plugins/git-pull.lua
+++ plugins/git-pull.lua
@@ -1,8 +1,8 @@
 return {
 	PRIVMSG = function(irc, sender, origin, message, pm)
 		if message:match("^!git pull") then
-			local res = os.execute("git pull --ff-only")
-			local msg = sender[1] .. ": git pull complete: " .. tostring(res)
+			local _, _, code = os.execute("git pull --ff-only")
+			local msg = sender[1] .. ": git pull complete: " .. tostring(code)
 			irc:PRIVMSG(origin, msg)
 		end
 	end;
