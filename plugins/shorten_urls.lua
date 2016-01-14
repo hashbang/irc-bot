8302d890e284eff30bada47edd7bdef7ee64a579
daurnimator <quae@daurnimator.com>
2016-01-14 14:18:20 +1100

    plugins/shorten_urls: Shorten multiple urls in a message

--- plugins/shorten_urls.lua
+++ plugins/shorten_urls.lua
@@ -14,14 +14,16 @@ local http_patt = "https?://[%w./%?%%+#_:;[%]%-!~*'()@&=%$,]+"
 
 return {
 	PRIVMSG = function(irc, sender, origin, message, pm)
-		local url = message:match(http_patt)
-		if not url then return end
-		-- Don't get in a loop
-		if #url < 22 then return end
-		-- Just in case v.gd urls get longer one day
-		if url:match("https?://v.gd/") then return end
-		local short = shorten(url)
-		local msg = string.format("%s: Shortened < %s >", sender[1], short)
-		irc:PRIVMSG(origin, msg)
+		for url in message:gmatch(http_patt) do
+			-- Don't get in a loop with multiple bots
+			if #url >= 22 and
+				-- Just in case v.gd urls get longer one day
+				not url:match("https?://v.gd/")
+			then
+				local short = shorten(url)
+				local msg = string.format("%s: Shortened < %s >", sender[1], short)
+				irc:PRIVMSG(origin, msg)
+			end
+		end
 	end;
 }
