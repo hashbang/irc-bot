9fffa9e35dcf5fccfaee29451d891143845704ad
daurnimator <quae@daurnimator.com>
2017-05-01 07:46:30 +0000

    plugins/reload: Disabled until #11 is fixed

--- plugins/reload.lua
+++ plugins/reload.lua
@@ -2,6 +2,10 @@ return {
 	hooks = {
 		PRIVMSG = function(irc, state, sender, origin, message, pm) -- luacheck: ignore 212
 			if message:match("^!reload") then
+				if true then
+					irc:PRIVMSG(origin, sender[1] .. ": sorry, but reloading is disabled until https://github.com/hashbang/irc-bot/issues/11 is solved")
+					return
+				end
 				irc:PRIVMSG(origin, "Reloading at request of " .. sender[1])
 				irc:reload_plugins()
 			end
