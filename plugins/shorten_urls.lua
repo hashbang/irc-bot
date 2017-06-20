787620aa31100f64ca8affca61758612f2e40740
tilpner <till@hoeppner.ws>
2017-06-21 00:37:03 +0200

    Don't highlight of user during url-shortening
    This is completely unnecessary: It will only ping me when I
    just sent a URL, and I'll already know I sent a URL, there's
    no need to remind me about that.
    
    Insert a no-width-space after the first character of the users
    nick in order to prevent their client from highlighting them.
    
    Send NOTICE instead of PRIVMSG to avoid bot-loops.

--- plugins/shorten_urls.lua
+++ plugins/shorten_urls.lua
@@ -74,8 +74,12 @@ return {
 					end
 				end
 				if #msg > 0 then
-					msg = sender[1] .. ": " .. msg
-					irc:PRIVMSG(origin, msg)
+					-- Prevent the user's client from triggering a notification,
+					-- by including a zero-width space character in their name
+					-- (that character might be invisible in your editor)
+					local nick = sender[1]:gsub("^(.)(.*)$", "%1​%2")
+					msg = nick .. ": " .. msg
+					irc:NOTICE(origin, msg)
 				end
 			end
 		end;
