a918a943e765b1ae6bc20c9ce1a05fdc2c76c3ee
daurnimator <quae@daurnimator.com>
2018-08-11 14:07:41 +1000

    plugins/helpnoob.lua: consistent indentation

--- plugins/helpnoob.lua
+++ plugins/helpnoob.lua
@@ -1,17 +1,17 @@
 local knowledge_base = {
-  hb = "The IRC client we use is weechat ( https://weechat.org/files/doc/stable/weechat_quickstart.en.html ), running in the tmux terminal multiplexer ( https://www.tmuxcheatsheet.com/ )";
+	hb = "The IRC client we use is weechat ( https://weechat.org/files/doc/stable/weechat_quickstart.en.html ), running in the tmux terminal multiplexer ( https://www.tmuxcheatsheet.com/ )";
 }
 
 return {
-  hooks = {
-    PRIVMSG = function(irc, state, sender, origin, message, pm) -- luacheck: ignore 212
-      local topic = message:match("^!explain%s+(%S+)")
-      if not topic then return end
-      local answer = knowledge_base[topic]
-      if answer == nil then
-        answer = "I don't know about that topic. Submit a pull request!"
-      end
-      irc:NOTICE(origin, answer)
-    end;
-  }
+	hooks = {
+		PRIVMSG = function(irc, state, sender, origin, message, pm) -- luacheck: ignore 212
+			local topic = message:match("^!explain%s+(%S+)")
+			if not topic then return end
+			local answer = knowledge_base[topic]
+			if answer == nil then
+				answer = "I don't know about that topic. Submit a pull request!"
+			end
+			irc:NOTICE(origin, answer)
+		end;
+	}
 }
