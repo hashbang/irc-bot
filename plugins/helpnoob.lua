26cc0ac8e4611a154ee23c9a730773ebcdceceba
daurnimator <quae@daurnimator.com>
2018-08-11 13:57:38 +1000

    plugins/helpnoob.lua: Fix invalid lua

--- plugins/helpnoob.lua
+++ plugins/helpnoob.lua
@@ -1,17 +1,16 @@
 local knowledge_base = {
-  ["hb"] = "The IRC client we use is weechat ( https://weechat.org/files/doc/stable/weechat_quickstart.en.html ), running in the tmux terminal multiplexer ( https://www.tmuxcheatsheet.com/ )."
+  hb = "The IRC client we use is weechat ( https://weechat.org/files/doc/stable/weechat_quickstart.en.html ), running in the tmux terminal multiplexer ( https://www.tmuxcheatsheet.com/ )"
 }
 
 return {
   hooks = {
     PRIVMSG = function(irc, state, sender, origin, message, pm) -- luacheck: ignore 212
       local topic = message:match("^!explain%s+(%S+)")
-      if topic then
-        if topic in knowledge_base then
-          irc:NOTICE(origin, knowledge_base[topic])
-        else
-          irc:NOTICE(origin, "I don't know about that topic. Submit a pull request!")
-        end
+      if not topic then return end
+      local answer = knowledge_base[topic]
+      if answer == nil then
+        answer = "I don't know about that topic. Submit a pull request!"
       end
+      irc:NOTICE(origin, answer)
   }
 }
